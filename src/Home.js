8f129f0330194ef7e40f2998c6badd8df3ff5280
unknown <dubemobiwon@gmail.com>
2023-04-21 03:33:15 -0700

    Commit

---
+++ src/Home.js
@@ -0,0 +1,28 @@
+import { useState,} from "react";
+import BlogList from "./BlogList";
+import useFetch from "./useFetch"
+
+const Home = () => {
+    const {data: blogs, isPending, error} = useFetch("  http://localhost:8000/blogs")
+  
+    const [name, setName] = useState("mario")
+    
+    // const handleDelete = (id)=>{
+    //     const newBlogs = blogs.filter(blog => blog.id !== id)
+    //     setBlogs(newBlogs)
+    // }
+    
+
+    return ( 
+        <div className="home">
+            {error && <div>{error}</div>}
+            {isPending && <div>Loading...</div>}
+          {blogs && <BlogList blogs={blogs} title="All Blogs" />}
+          <button onClick={()=> setName('Dubem')}>Change name</button>
+          <p>{name}</p>
+          {/* <BlogList blogs={blogs.filter(blog=> blog.author === 'mario')} title="Mario's Blogs"/> */}
+        </div>
+     );
+}
+ 
+export default Home;
